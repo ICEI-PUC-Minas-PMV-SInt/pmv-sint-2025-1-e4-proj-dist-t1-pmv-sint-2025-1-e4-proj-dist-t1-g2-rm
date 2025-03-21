c70a8aca1f84252244899cb68b7728c08b32ec40
Geisson Oliveira <geissonlucaso@gmail.com>
2025-03-21 11:00:34 -0300

    Definição da API e do endpoind GetAll para Produtos.

---
+++ src/todoz.api/ReciclaMaisAPI/ReciclaMaisAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReciclaMaisAPI.Data;
+
+namespace ReciclaMaisAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var model = await _context.ProdutosResiduos.ToListAsync();
+
+            return Ok(model);
+        }
+    }
+}
