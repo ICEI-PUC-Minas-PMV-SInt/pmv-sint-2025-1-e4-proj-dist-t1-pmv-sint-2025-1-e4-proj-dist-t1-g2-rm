18ef3848f72aa9a1d08822d8f96c31c1e9b6e9ee
Geisson Oliveira <geissonlucaso@gmail.com>
2025-03-21 13:15:04 -0300

    Definição dos endpoints GetById e Create de ProdutosResiduos.

--- src/todoz.api/ReciclaMaisAPI/ReciclaMaisAPI/Controllers/ProdutosController.cs
+++ src/todoz.api/ReciclaMaisAPI/ReciclaMaisAPI/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ReciclaMaisAPI.Data;
+using ReciclaMaisAPI.Models;
 
 namespace ReciclaMaisAPI.Controllers
 {
@@ -20,6 +21,31 @@ namespace ReciclaMaisAPI.Controllers
         public async Task<IActionResult> GetAll()
         {
             var model = await _context.ProdutosResiduos.ToListAsync();
+
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ProdutoResiduo model)
+        {
+            if (model.Pontuacao <= 0)
+            {
+                return BadRequest(new { message = "A pontução deve ser maior do que zero." });
+            }
+
+            _context.ProdutosResiduos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.ProdutosResiduos
+                .FirstOrDefaultAsync(v => v.Id == id);
+
+            if (model == null) NotFound();
 
             return Ok(model);
         }
