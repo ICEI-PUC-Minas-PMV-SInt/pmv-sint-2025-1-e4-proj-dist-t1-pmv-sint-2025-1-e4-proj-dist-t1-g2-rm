5b57dd040db91ecdd6c9707c30fe111e3dee3c86
piconina <lorenaterenzi@gmail.com>
2025-04-11 19:01:59 -0300

    productdetails

--- src/todoz.web/ReciclaMaisReact/recicla-mais-frontend/src/App.js
+++ src/todoz.web/ReciclaMaisReact/recicla-mais-frontend/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Recicla Mais - Produtos</h1>
-      <ProductList />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Recicla Mais - Produtos</h1>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/detalhes/:id" element={<ProductDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/todoz.web/ReciclaMaisReact/recicla-mais-frontend/src/components/ProductDetails.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
+const ProductDetails = () => {
+  const { id } = useParams();
+  const [produto, setProduto] = useState(null);
+  const [erro, setErro] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`https://localhost:7215/api/Produtos/${id}`)
+      .then((response) => {
+        setProduto(response.data);
+        setErro("");
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar produto:", error);
+        setErro("Produto não encontrado.");
+      });
+  }, [id]);
+
+  return (
+    <div className="get-container">
+      <h2>Detalhes do Produto</h2>
+
+      {erro && <p className="erro">{erro}</p>}
+
+      {produto && (
+        <div className="produto-info">
+          <p>
+            <strong>ID:</strong> {produto.id}
+          </p>
+          <p>
+            <strong>Nome:</strong> {produto.nome}
+          </p>
+          <p>
+            <strong>Descrição:</strong> {produto.descricao}
+          </p>
+          <p>
+            <strong>Pontuação:</strong> {produto.pontuacao}
+          </p>
+        </div>
+      )}
+
+      <button
+        onClick={() => navigate(-1)}
+        className="buscar-button"
+        style={{ marginTop: "20px" }}
+      >
+        Voltar
+      </button>
+    </div>
+  );
+};
+
+export default ProductDetails;
